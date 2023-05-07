7dafbb5e0ab6a4b4059bcb99e76e3712ecf66e07
nuci <kirianuci@gmail.com>
2023-05-07 22:52:26 +0400

    Add hero section design

--- vite-projec/src/page/Hero.jsx
+++ vite-projec/src/page/Hero.jsx
@@ -1,12 +1,25 @@
-import "../style/hero.css"
+import "../style/hero.css";
+import heroRectangleImage from "../image/hero-image.png";
 
 const Hero = () => {
   return (
     <div className="hero_container">
-     <div></div>
-     <div></div>
+      <div className="hero_left">
+        <div className="hero_content">
+          <h4 className="hero_title">Ingenieur Dieter Weiss</h4>
+          <h2>Ingenieurbüro für Bau- und Vermessungswesen</h2>
+          <p className="hero_text">
+            Wenn Sie zuverlässige Ingenieure brauchen – das Ingenieurbüro Dieter
+            Weiss ist Garant für den Er­folg Ihrer Projekte.
+          </p>
+          <button className="hero_btn">Ihr Projekt mit uns</button>
+        </div>
+      </div>
+      <div className="hero_right">
+        <img src={heroRectangleImage} alt="Company photo in the hero section" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
