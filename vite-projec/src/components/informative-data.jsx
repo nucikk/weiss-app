0d4d7b3eb628d13afdfc9ea3b1e00fa1a76a11d8
nuci <kirianuci@gmail.com>
2023-05-09 21:20:53 +0400

    Add articles section to website

---
+++ vite-projec/src/components/informative-data.jsx
@@ -0,0 +1,32 @@
+import StudiesPhoto from "../image/article-image-1.png"
+import ConceptsPhoto from "../image/article-image-2.png"
+import ResearchPhoto from "../image/article-image-3.png"
+
+ const informativeData = [
+    {
+      id: 1,
+      photo: StudiesPhoto,
+      title: "Studien und Konzepte, Gesamtplanung",
+      description:
+        "Neubau und Ausbau von Straßen, Verkehrsflächen, Wasserwegen und Eisenbahnen require convincing Verkehrskonzepte.",
+      link: "Erfahren Sie mehr"
+    },
+    {
+      id: 2,
+      photo: ConceptsPhoto,
+      title: "Verkehrsanlagen und Ingenieurbauwerke",
+      description:
+        "Verkehrskonzepte sind ein entscheidender Faktor jeglicher Infrastruktur. Das Ingenieurbüro Weiss has therefore set up an expert team.",
+      link: "Erfahren Sie mehr"
+    },
+    {
+      id: 3,
+      photo: ResearchPhoto,
+      title: "Entwurfsvermessung, 3D-Laserscanning",
+      description:
+        "Bei Ausführung sämtlicher vermessungstechnischer Aufgaben im Leistungsbild 'Entwurfsvermessung' sind wir ein poweglicher Partner.",
+      link: "Erfahren Sie mehr"
+    }
+  ];
+
+  export default informativeData
--- vite-projec/src/page/Service.jsx
+++ vite-projec/src/page/Service.jsx
@@ -1,15 +1,38 @@
-import "../style/Service.css"
+import "../style/Service.css";
+import informativeData from "../components/informative-data";
+
 const Service = () => {
   return (
     <div className="service_container">
-    <div className="service_content">
+      <div className="service_content">
         <div className="service_description">
-            <h4>Unsere Dienstleistungen</h4>
-            <p>Dienstleistung ist für unsere Mitarbeiter Herausforderung, das ist die Stärke unseres Teams. Ganz gleich, ob Verkehrsplanungen, Ver- und Ent­sorgung, Lärmschutz oder Spezialgebiete – wir bieten Rundum-Service mit:</p>
+          <h4>Unsere Dienstleistungen</h4>
+          <p>
+            Dienstleistung ist für unsere Mitarbeiter Herausforderung, das ist
+            die Stärke unseres Teams. Ganz gleich, ob Verkehrsplanungen, Ver-
+            und Ent­sorgung, Lärmschutz oder Spezialgebiete – wir bieten
+            Rundum-Service mit:
+          </p>
         </div>
-    </div>
-    </div>
-  )
-}
 
-export default Service
+        <div className="service_article">
+          {informativeData.map((item) => (
+            <div className="informative_box" key={item.id}>
+              <img src={item.photo} alt={item.title} />
+
+              <div className="informative_content">
+                <h3>{item.title}</h3>
+                <p className="informative_description">{item.description}</p>
+                <a className="informative_link" href="#">
+                  {item.link}
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
